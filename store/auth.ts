4466e47326ea8ff7541c051bbf22a480eee01192
presuit <chldudgh8256@naver.com>
2023-01-11 15:41:20 +0900

    fix:AuthMode 타입을 types 폴더로 이동

--- store/auth.ts
+++ store/auth.ts
@@ -1,7 +1,7 @@
 import { atom } from "recoil";
 import { AUTH_TOKEN } from "../constants/auth";
+import { TAuthMode } from "../types/auth";
 
-export type TAuthMode = "SIGN_IN" | "SIGN_UP";
 export const authModeState = atom<TAuthMode>({
   key: "authModeState",
   default: "SIGN_IN",
---
+++ types/auth.ts
@@ -0,0 +1,1 @@
+export type TAuthMode = "SIGN_IN" | "SIGN_UP";
